54fdc1bf517b9ffa09fad3bbfc0635b877331d04
xalgord <karan@xalgorix.com>
2026-03-17 01:27:30 +0530

    v1.2.0 - Fix dashboard logo path

--- cmd/xalgorix/main.go
+++ cmd/xalgorix/main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/xalgord/xalgorix/internal/web"
 )
 
-const version = "1.1.9"
+const version = "1.2.0"
 
 func main() {
 	args := parseArgs()
--- internal/web/server.go
+++ internal/web/server.go
@@ -29,7 +29,7 @@ import (
 	"github.com/xalgord/xalgorix/internal/tools/reporting"
 )
 
-const version = "1.1.9"
+const version = "1.2.0"
 
 //go:embed static/*
 var staticFiles embed.FS
