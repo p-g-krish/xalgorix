899d67de759385b97c8fd1e2549924f521014913
xalgord <karan@xalgorix.com>
2026-03-21 00:03:18 +0530

    v1.4.11 - AgentMail CSS specificity fix

--- cmd/xalgorix/main.go
+++ cmd/xalgorix/main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/xalgord/xalgorix/internal/web"
 )
 
-const version = "1.4.10"
+const version = "1.4.11"
 
 func main() {
 	args := parseArgs()
--- internal/web/server.go
+++ internal/web/server.go
@@ -29,7 +29,7 @@ import (
 	"github.com/xalgord/xalgorix/internal/tools/reporting"
 )
 
-const version = "1.4.10"
+const version = "1.4.11"
 
 //go:embed static/*
 var staticFiles embed.FS
