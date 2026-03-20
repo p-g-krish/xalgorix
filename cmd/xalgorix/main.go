d414bc4feb95f9d41e47bd61da43e3746d4f5a01
xalgord <karan@xalgorix.com>
2026-03-20 23:28:15 +0530

    v1.4.4 - Fix AgentMail layout

--- cmd/xalgorix/main.go
+++ cmd/xalgorix/main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/xalgord/xalgorix/internal/web"
 )
 
-const version = "1.4.3"
+const version = "1.4.4"
 
 func main() {
 	args := parseArgs()
--- internal/web/server.go
+++ internal/web/server.go
@@ -29,7 +29,7 @@ import (
 	"github.com/xalgord/xalgorix/internal/tools/reporting"
 )
 
-const version = "1.4.3"
+const version = "1.4.4"
 
 //go:embed static/*
 var staticFiles embed.FS
