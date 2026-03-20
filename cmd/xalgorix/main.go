c25c284c184b167ce9a1679cd06bd0f5fd7d9263
xalgord <karan@xalgorix.com>
2026-03-21 00:00:42 +0530

    v1.4.10 - Fix AgentMail selector

--- cmd/xalgorix/main.go
+++ cmd/xalgorix/main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/xalgord/xalgorix/internal/web"
 )
 
-const version = "1.4.9"
+const version = "1.4.10"
 
 func main() {
 	args := parseArgs()
--- internal/web/server.go
+++ internal/web/server.go
@@ -29,7 +29,7 @@ import (
 	"github.com/xalgord/xalgorix/internal/tools/reporting"
 )
 
-const version = "1.4.9"
+const version = "1.4.10"
 
 //go:embed static/*
 var staticFiles embed.FS
