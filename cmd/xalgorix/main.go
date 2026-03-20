fa90c7fd04fddfa3c02b3123ed541cc06a9bb0a4
xalgord <karan@xalgorix.com>
2026-03-20 23:41:14 +0530

    v1.4.7 - Fix AgentMail full width

--- cmd/xalgorix/main.go
+++ cmd/xalgorix/main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/xalgord/xalgorix/internal/web"
 )
 
-const version = "1.4.6"
+const version = "1.4.7"
 
 func main() {
 	args := parseArgs()
--- internal/web/server.go
+++ internal/web/server.go
@@ -29,7 +29,7 @@ import (
 	"github.com/xalgord/xalgorix/internal/tools/reporting"
 )
 
-const version = "1.4.6"
+const version = "1.4.7"
 
 //go:embed static/*
 var staticFiles embed.FS
